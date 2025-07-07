cfccb12ed319120bd372d00ea36c7b49622ff1fe
jatoapan <jatoapan@espol.edu.ec>
2025-07-07 16:38:21 -0500

    re starting

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,30 +2,33 @@ import { Grid } from '@mui/material';
 
 function App() {
    return (
-       <Grid container spacing={5} justifyContent="center" alignItems="center">
+      <div>
+          <h1>Bienvenido al Dashboard</h1>
+          <Grid container spacing={5} justifyContent="center" alignItems="center">
 
-         {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+            {/* Encabezado */}
+            <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
-         {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+            {/* Alertas */}
+            <Grid>Elemento: Alertas</Grid>
 
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Selector</Grid>
+            {/* Selector */}
+            <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
 
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Indicadores</Grid>
+            {/* Indicadores */}
+            <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
-         {/* Gráfico */}
-         <Grid size={{ xs: 12, md: 12 }} sx={{ display: { xs: "none", md: "block"} }} >Elemento: Gráfico</Grid>
+            {/* Gráfico */}
+            <Grid sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
 
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 12 }} sx={{ display: { xs: "none", md: "block"} }} >Elemento: Tabla</Grid>
+            {/* Tabla */}
+            <Grid sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
 
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+            {/* Información adicional */}
+            <Grid>Elemento: Información adicional</Grid>
 
-      </Grid>
+          </Grid>
+      </div>
    );
 }
 

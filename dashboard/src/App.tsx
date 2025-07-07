f8185503bc7c872c7600afcd6910a0da6f086b38
jatoapan <jatoapan@espol.edu.ec>
2025-07-07 14:30:45 -0500

    changes

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+   return (
+       <Grid container spacing={5} justifyContent="center" alignItems="center">
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+         {/* Encabezado */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid size={{ xs: 12, md: 12 }} sx={{ display: { xs: "none", md: "block"} }} >Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid size={{ xs: 12, md: 12 }} sx={{ display: { xs: "none", md: "block"} }} >Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+      </Grid>
+   );
 }
 
-export default App
+export default App;

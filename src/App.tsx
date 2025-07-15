86115ada58e4f4a22a64f24833c0e0fcd82e1e52
nfiallo5 <nfiallo@espol.edu.ec>
2025-07-14 19:04:39 -0500

    package.json modified and PWA added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Grid } from '@mui/material';
-import './App.css';
-import HeaderUI from './components/HeaderUI';
-import AlertUI from './components/AlertUI';
-import SelectorUI from './components/SelectorUI';
-import IndicatorUI from './components/IndicatorUI';
-import DataFetcher from './functions/DataFetcher';
-import TableUI from './components/TableUI';
-import ChartUI from './components/ChartUI';
-import { useState } from 'react';
+import { Grid } from "@mui/material";
+import "./App.css";
+import HeaderUI from "./components/HeaderUI";
+import AlertUI from "./components/AlertUI";
+import SelectorUI from "./components/SelectorUI";
+import IndicatorUI from "./components/IndicatorUI";
+import DataFetcher from "./functions/DataFetcher";
+import TableUI from "./components/TableUI";
+import ChartUI from "./components/ChartUI";
+import { useState } from "react";
 
 function App() {
-  const [city, setCity] = useState<string>('guayaquil');
+  const [city, setCity] = useState<string>("guayaquil");
 
   const dataFetcherOutput = DataFetcher(city);
 
@@ -25,8 +25,12 @@ function App() {
         </Grid>
 
         {/* Alertas */}
-        <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
-          Elemento: Alertas
+        <Grid
+          size={{ xs: 12, md: 12 }}
+          container
+          justifyContent="right"
+          alignItems="center"
+        >
           <AlertUI description="No se preveen lluvias" />
         </Grid>
 
@@ -36,7 +40,7 @@ function App() {
         </Grid>
 
         {/* Indicadores */}
-        <Grid container size={{ xs: 12, md: 9 }} >
+        <Grid container size={{ xs: 12, md: 9 }}>
           {/* Elemento: Indicadores */}
           {/* Renderizado condicional de los datos obtenidos */}
 
@@ -46,56 +50,88 @@ function App() {
             <>
               {/* Indicadores con datos obtenidos */}
 
-              <Grid size={{ xs: 12, md: 3 }} >
+              <Grid size={{ xs: 12, md: 3 }}>
                 <IndicatorUI
-                  title='Temperatura (2m)'
-                  description={dataFetcherOutput.data.current.temperature_2m + " " + dataFetcherOutput.data.current_units.temperature_2m} />
+                  title="Temperatura (2m)"
+                  description={
+                    dataFetcherOutput.data.current.temperature_2m +
+                    " " +
+                    dataFetcherOutput.data.current_units.temperature_2m
+                  }
+                />
               </Grid>
 
               <Grid size={{ xs: 12, md: 3 }}>
                 <IndicatorUI
-                  title='Temperatura aparente'
-                  description={dataFetcherOutput.data.current.apparent_temperature + " " + dataFetcherOutput.data.current_units.apparent_temperature} />
+                  title="Temperatura aparente"
+                  description={
+                    dataFetcherOutput.data.current.apparent_temperature +
+                    " " +
+                    dataFetcherOutput.data.current_units.apparent_temperature
+                  }
+                />
               </Grid>
 
               <Grid size={{ xs: 12, md: 3 }}>
                 <IndicatorUI
-                  title='Velocidad del viento'
-                  description={dataFetcherOutput.data.current.wind_speed_10m + " " + dataFetcherOutput.data.current_units.wind_speed_10m} />
+                  title="Velocidad del viento"
+                  description={
+                    dataFetcherOutput.data.current.wind_speed_10m +
+                    " " +
+                    dataFetcherOutput.data.current_units.wind_speed_10m
+                  }
+                />
               </Grid>
 
               <Grid size={{ xs: 12, md: 3 }}>
                 <IndicatorUI
-                  title='Humedad relativa'
-                  description={dataFetcherOutput.data.current.relative_humidity_2m + " " + dataFetcherOutput.data.current_units.relative_humidity_2m} />
+                  title="Humedad relativa"
+                  description={
+                    dataFetcherOutput.data.current.relative_humidity_2m +
+                    " " +
+                    dataFetcherOutput.data.current_units.relative_humidity_2m
+                  }
+                />
               </Grid>
-
             </>
           )}
         </Grid>
 
         {/* Gráfico */}
-        <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+        <Grid
+          size={{ xs: 6, md: 6 }}
+          sx={{ display: { xs: "none", md: "block" } }}
+        >
           {dataFetcherOutput.loading && <p>Cargando datos...</p>}
           {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
-          {dataFetcherOutput.data && (<ChartUI hourly={dataFetcherOutput.data.hourly} hourly_units={dataFetcherOutput.data.hourly_units}/>
-        )}
+          {dataFetcherOutput.data && (
+            <ChartUI
+              hourly={dataFetcherOutput.data.hourly}
+              hourly_units={dataFetcherOutput.data.hourly_units}
+            />
+          )}
         </Grid>
 
         {/* Tabla */}
-        <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+        <Grid
+          size={{ xs: 6, md: 6 }}
+          sx={{ display: { xs: "none", md: "block" } }}
+        >
           {dataFetcherOutput.loading && <p>Cargando datos...</p>}
           {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
-          {dataFetcherOutput.data && 
-          (<TableUI hourly={dataFetcherOutput.data.hourly} hourly_units={dataFetcherOutput.data.hourly_units}/>)}
+          {dataFetcherOutput.data && (
+            <TableUI
+              hourly={dataFetcherOutput.data.hourly}
+              hourly_units={dataFetcherOutput.data.hourly_units}
+            />
+          )}
         </Grid>
 
         {/* Información adicional */}
         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
-
       </Grid>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
